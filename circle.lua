8a5eb310a10c1bb821dc61548765b2161cad35ee
itsweekly <92674764+itsweekly@users.noreply.github.com>
2022-01-29 09:42:17 +0100

    Update circle.lua

--- circle.lua
+++ circle.lua
@@ -36,11 +36,11 @@ local tableinsert = table.insert
 -- // Silent Aim Vars
 getgenv().Aiming = {
     Enabled = true,
-    ShowFOV = false,
+    ShowFOV = true,
     FOVSides = 15,
     FOVColour = Color3fromRGB(231, 84, 128),
     VisibleCheck = true,
-    FOV = 65,
+    FOV = 72,
     HitChance = 100,
     Selected = LocalPlayer,
     SelectedPart = nil,
